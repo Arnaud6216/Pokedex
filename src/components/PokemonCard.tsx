3e2337a4ccb5631e9cab800d99d0c61478e0f74f
Arnaud6216 <guevaer8@gmail.com>
2024-10-22 11:50:00 +0200

    adding props to pokemonCard

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,11 +1,12 @@
-function PokemonCard() {
+function PokemonCard({ pokemon }) {
 	return (
 		<figure>
-			<img
-				src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-				alt="Bulbasaur"
-			/>
-			<figcaption>Bulbasaur</figcaption>
+			{pokemon.imgSrc ? (
+				<img src={pokemon.imgSrc} alt={pokemon.name}></img>
+			) : (
+				"???"
+			)}
+			<figcaption>{pokemon.name}</figcaption>
 		</figure>
 	);
 }
